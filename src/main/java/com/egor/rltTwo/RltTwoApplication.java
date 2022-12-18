d52b7d398d456f099da79046d2ce9404ecb1ba42
nichitadolinschi <nichita.dolinschi@amdaris.com>
2022-12-18 15:18:21 +0200

    usefull coment

--- src/main/java/com/egor/rltTwo/RltTwoApplication.java
+++ src/main/java/com/egor/rltTwo/RltTwoApplication.java
@@ -3,6 +3,7 @@ package com.egor.rltTwo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Add spring security
 @SpringBootApplication
 public class RltTwoApplication {
 
