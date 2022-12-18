fb5a8be9c16df1a1e9fb6be3596a96345b333ae1
Mac <mac@MacBook-Pro.local>
2022-12-18 15:23:14 +0200

    add a field

--- src/main/java/com/egor/rltTwo/RltTwoApplication.java
+++ src/main/java/com/egor/rltTwo/RltTwoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Add spring security
+//Add spring securitysds
 @SpringBootApplication
 public class RltTwoApplication {
 
